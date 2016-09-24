90c6695fbcce9b9442640e9090a0c285d0a21b5a
Peter Lodri <silentpocok@gmail.com>
2016-09-24 10:12:52 +0200

    allin

--- player.js
+++ player.js
@@ -10,7 +10,7 @@ module.exports = {
     // var in_action = game_state[in_action];
     // var hand = [game_state.players[in_action]['hole_cards'][0]['rank'], game_state.players[in_action]['hole_cards'][1]['rank']];
     // console.log(hand);
-    bet(900);
+    bet(1000);
     console.log( 'game state + bet' );
     console.log( game_state );
     console.log( bet );
