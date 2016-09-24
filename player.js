fdc2af671d77f9bcfe5259c607627d1c7802f075
Peter Lodri <silentpocok@gmail.com>
2016-09-24 10:36:25 +0200

    get hand

--- player.js
+++ player.js
@@ -12,7 +12,11 @@ module.exports = {
     hand.push(game_state.players[0].hole_cards[0].rank);
     hand.push(game_state.players[0].hole_cards[1].rank);
     console.log(hand);
-    bet(1000);
+    if (goodStartingHand.indexOf(hand[0]) > -1 || goodStartingHand.indexOf(hand[1]) > -1) {
+      bet(1000);
+    }else {
+      bet(0);
+    }
     console.log( 'game state + bet' );
     console.log( game_state );
     console.log( bet );
