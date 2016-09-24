f75b237a34c6c19aad3ac5ac2faa600178ab6a86
Lyapunov <vaskorichard@gmail.com>
2016-09-24 10:28:06 +0200

    Change.

--- player.js
+++ player.js
@@ -6,9 +6,8 @@ module.exports = {
 
   bet_request: function(game_state, bet) {
     var hand;
-    var in_action;
+    var in_action = game_state.in_action;
     var goodStartingHand = ['10', 'J', 'K', 'Q', 'A'];
-    var in_action = game_state[in_action];
     hand.push(game_state.players[0].hole_cards[0].rank);
     hand.push(game_state.players[0].hole_cards[1].rank);
     console.log(in_action);
