f1a0850d4a733a936bb1c783a292528b0f3fd3c2
attilakrupl <krupl.attila@gmail.com>
2016-09-24 12:44:33 +0100

    tuning

--- player.js
+++ player.js
@@ -3,35 +3,33 @@ module.exports = {
 
   VERSION: "Default JavaScript folding player",
 
-  number_of_active_players: function(players) {
-    var num_of_players = 0;
-    for (var i = 0; i < players.length; i++) {
-      if(players[i].status != "out") {
-        num_of_players++;
-      }
-    }
-    return num_of_players;
-  },
-
-
-
-  prepare_bet: function(hand, player_num, check_amount) {
-    var cards = { "2" : 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8" : 8, "9" : 9, "10" : 10, "J" : 11, "Q": 12, "K": 13, "A": 14};
-    var goodStartingHand = ["10", "A", "J", "Q", "K"];
-
-    if (player_num > 3) {
-      if (goodStartingHand.indexOf(hand[0]) > -1 || goodStartingHand.indexOf(hand[1]) > -1) {
-          return 6000;
-      } else {
-        return 0;
-      }
-    } else {
-      return check_amount;
-    }
-},
-
-
   bet_request: function(game_state, bet) {
+
+    var number_of_active_players = function(players) {
+      var num_of_players = 0;
+      for (var i = 0; i < players.length; i++) {
+        if(players[i].status != "out") {
+          num_of_players++;
+        }
+      }
+      return num_of_players;
+    };
+
+    var prepare_bet = function(hand, player_num, check_amount) {
+        var cards = { "2" : 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8" : 8, "9" : 9, "10" : 10, "J" : 11, "Q": 12, "K": 13, "A": 14};
+        var goodStartingHand = ["10", "A", "J", "Q", "K"];
+
+        if (player_num > 3) {
+          if (goodStartingHand.indexOf(hand[0]) > -1 || goodStartingHand.indexOf(hand[1]) > -1) {
+              return 6000;
+          } else {
+            return 0;
+          }
+        } else {
+          return check_amount;
+        }
+    };
+
     var check_amount = game_state.current_buy_in - game_state.players[0].bet;
     var hand = [];
     hand.push(player.hole_cards[0].rank);
