8ac6cd1ded07dda456f2e26730a7592a043a2296
Peter Lodri <silentpocok@gmail.com>
2016-09-24 09:56:27 +0200

    allin

--- player.js
+++ player.js
@@ -4,10 +4,14 @@ module.exports = {
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state, bet) {
-    bet(1000);
+    var in_action = game_state.in_action;
+    var hand = game_state['players'][0][];
+    bet(900);
   },
 
   showdown: function(game_state) {
 
   }
+
+
 };
