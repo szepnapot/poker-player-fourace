5beaa63c16f2752c427cb6a9f5f6507b2398e511
Peter Lodri <silentpocok@gmail.com>
2016-09-24 10:10:35 +0200

    fix

--- player.js
+++ player.js
@@ -8,7 +8,7 @@ module.exports = {
   bet_request: function(game_state, bet) {
     // var in_action = game_state[in_action];
     // var hand = [game_state.players[in_action]['hole_cards'][0]['rank'], game_state.players[in_action]['hole_cards'][1]['rank']];
-    console.log(hand);
+    // console.log(hand);
     bet(900);
     console.log( 'game state + bet' );
     console.log( game_state );
