26a0b699d8caf79260ef100979d531346543d4af
Peter Lodri <silentpocok@gmail.com>
2016-09-24 10:02:58 +0200

    start hand

--- player.js
+++ player.js
@@ -3,9 +3,11 @@ module.exports = {
 
   VERSION: "Default JavaScript folding player",
 
+  const goodStartingHand = ['10', 'J', 'K', 'Q', 'A'];
+
   bet_request: function(game_state, bet) {
     var in_action = game_state.in_action;
-    var hand = game_state.players[in_action]['hole_cards'];
+    var hand = game_state.players[in_action]['hole_cards']['rank'];
     console.log(hand);
     bet(900);
     console.log( 'game state + bet' );
