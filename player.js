dc80712f49554e4eb6f28efff53ad15a8612e678
Peter Lodri <silentpocok@gmail.com>
2016-09-24 10:20:57 +0200

    get hand

--- player.js
+++ player.js
@@ -9,8 +9,8 @@ module.exports = {
     var in_action;
     var goodStartingHand = ['10', 'J', 'K', 'Q', 'A'];
     var in_action = game_state[in_action];
-    var hand = [game_state.players[in_action]['hole_cards'][0]['rank'], game_state.players[in_action]['hole_cards'][1]['rank']];
-    console.log(game_state.players[in_action]['hole_cards'][0]['rank']);
+    // var hand = [game_state.players[in_action]['hole_cards'][0]['rank'], game_state.players[in_action]['hole_cards'][1]['rank']];
+    console.log(game_state.players[0]);
     console.log(in_action);
     bet(1000);
     console.log( 'game state + bet' );
