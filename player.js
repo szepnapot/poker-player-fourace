d3c2a429a4f87a30ab764478727942f1dfa60f3d
Lyapunov <vaskorichard@gmail.com>
2016-09-24 09:52:55 +0200

    Adding logging.

--- player.js
+++ player.js
@@ -5,6 +5,9 @@ module.exports = {
 
   bet_request: function(game_state, bet) {
     bet(1000);
+    console.log( 'game state + bet' );
+    console.log( game_state );
+    console.log( bet );
   },
 
   showdown: function(game_state) {
