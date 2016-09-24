bcbcdc15d993295ab472343df713f7569572e073
Peter Lodri <silentpocok@gmail.com>
2016-09-24 10:15:34 +0200

    fix

--- player.js
+++ player.js
@@ -1,12 +1,13 @@
 
 module.exports = {
 
-  VERSION: "Default JavaScript folding player";
-  var hand;
-  var in_action;
-  var goodStartingHand = ['10', 'J', 'K', 'Q', 'A'];
+  VERSION: "Default JavaScript folding player",
+
 
   bet_request: function(game_state, bet) {
+    var hand;
+    var in_action;
+    var goodStartingHand = ['10', 'J', 'K', 'Q', 'A'];
     var in_action = game_state[in_action];
     // var hand = [game_state.players[in_action]['hole_cards'][0]['rank'], game_state.players[in_action]['hole_cards'][1]['rank']];
     // console.log(hand);
