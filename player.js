c9e7b12effe2d98ad50bea9af3605e303af4f437
Peter Lodri <silentpocok@gmail.com>
2016-09-24 10:13:31 +0200

    inaction

--- player.js
+++ player.js
@@ -7,9 +7,10 @@ module.exports = {
   var goodStartingHand = ['10', 'J', 'K', 'Q', 'A'];
 
   bet_request: function(game_state, bet) {
-    // var in_action = game_state[in_action];
+    var in_action = game_state[in_action];
     // var hand = [game_state.players[in_action]['hole_cards'][0]['rank'], game_state.players[in_action]['hole_cards'][1]['rank']];
     // console.log(hand);
+    console.log(in_action);
     bet(1000);
     console.log( 'game state + bet' );
     console.log( game_state );
